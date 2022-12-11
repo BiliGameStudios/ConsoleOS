18f59625f8106822e3323e3d7f08ac14d62fe4e9
Zhuchen_pig <Zhuchen_pig@163.com>
2022-12-11 17:31:41 +0800

    ver 0.0.2

--- ConsoleOS/Command.h
+++ ConsoleOS/Command.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // include
-#include <string.h>
+#include <iostream>
 
 // using
 using namespace std;
@@ -13,8 +13,40 @@ string command;
 flag quit = false;
 
 // funcion
+int toUnicode(const char* str)
+{
+	return str[0] + (str[1] ? toUnicode(str + 1) : 0);
+}
+
+constexpr inline int caseString(const char* str)
+{
+	return str[0] + (str[1] ? caseString(str + 1) : 0);
+}
+
+void welcome()
+{
+	cout << "================================================" << endl;
+	cout << "====    ===    ==  === ==   ===   == ====     ==" << endl;
+	cout << "=== ====== ==== = = == = ===== === = ==== ======" << endl;
+	cout << "== ======= ==== = = == = ===== === = ====    ===" << endl;
+	cout << "== ======= ==== = == = ==   == === = ==== ======" << endl;
+	cout << "=== ====== ==== = == = ===== = === = ==== ======" << endl;
+	cout << "====    ===    == ===  ==   ===   ==    =     ==" << endl;
+	cout << "=============== Operating System ===============" << endl << endl;
+
+	cout << "System is booting, please wait..." << endl;
+
+	for (long long i = 0; i <= 10000000000LL; i++)
+	{
+		continue;
+	}
+
+	system("cls");
+}
+
 void commandEvent(string command)
 {
+	//shutdown system
 	if (command == "exit" || command == "quit" || command == "shutdown")
 	{
 		cout << "System is shutting down, please wait..." << endl;
--- ConsoleOS/Main.cpp
+++ ConsoleOS/Main.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
 #include "Command.h"
 
 using namespace std;
 
 int main()
 {
+	// system init
+	system("mode con cols=48 lines=20  ");
+	welcome();
+
+	// system
 	while (true)
 	{
 		cout << "<<<";
